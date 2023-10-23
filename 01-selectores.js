5af51a86bdbeb34478bb7c482c3404924764569f
Jomaro96 <56599883+Jomaro96@users.noreply.github.com>
2023-10-23 14:41:30 -0500

    Added CSS and some methods

--- 01-selectores.js
+++ 01-selectores.js
@@ -8,6 +8,18 @@ $(document).ready(function(){
              .css("color","black"); //metodo css tiene de parametros propiedad y valor, se pueden hacer cadenas del metodo
 
     //Selector de clases         
-    $(".zebra").css("border","5px dashed black");
+    //$(".zebra").css("border","5px dashed black");
+
+   $("#selector_class").children().click(function() {
+     if($(this).attr("class").includes("zebra")) //attr se utiliza para sacar el nombre de clase o id
+     {
+     $(this).removeClass("zebra"); //removeclass quita una o todas las clases si no se define
+     }
+     else 
+     {
+     $(this).addClass("zebra");
+     }
+   }); //Seleccione el hijo de mi DIV para que a ese hijo se le agregue la clase deseada
+
 }); //document ready Se espera a que cargue toda la pagina web
 // El $ se refiere a jquery, se puede usar eso o escribir jquery
